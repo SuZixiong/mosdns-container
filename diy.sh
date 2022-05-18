8a1fe4a14c0065d531574b6beb5a7e42d05ff09d
SuZixiong <szx422610241@gmail.com>
2022-05-18 12:33:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #将/config.yaml内ecs-cn,ecs-us替换
-sed -i 's/a.a.a.a/$ecs-cn/g' /config.yaml
-sed -i 's/b.b.b.b/$ecs-us/g' /config.yaml
+sed -i 's/12.12.12.12/$ecs-cn/g' /config.yaml
+sed -i 's/123.123.123.123/$ecs-us/g' /config.yaml
