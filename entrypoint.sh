c8c6997fc0a03f0c894e82125569bd8db1314d71
SuZixiong <szx422610241@gmail.com>
2022-05-18 18:20:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sed -i 's/12.12.12.12/$ecs-cn/g' /config.yaml
-sed -i 's/123.123.123.123/$ecs-us/g' /config.yaml
+sed -i 's/12.12.12.12/$ECS_CN/g' /config.yaml
+sed -i 's/123.123.123.123/$ECS-US/g' /config.yaml
 if [ ! -f /etc/mosdns/config.yaml ]; then
 	mkdir -p /etc/mosdns/
 	cp -u /config.yaml /etc/mosdns/config.yaml
